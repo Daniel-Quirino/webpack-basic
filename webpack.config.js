32dd81584315419fdbe36bcd43b94d29d968a762
Daniel Quirino <danielpires6@hotmail.com>
2022-02-13 17:11:56 -0300

    :sparkles: Added html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -23,6 +24,9 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin(),
+    ]
 
 
 }
